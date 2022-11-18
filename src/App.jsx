4b827e78c3f5569badbed28e8928a4364699e372
Himat Singh <singhimat99@gmail.com>
2022-11-18 12:00:51 -0800

    implemented lazy loading for chatroom and displaynae components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import SignIn from "./pages/SignIn.jsx";
-import ChatRoom from "./pages/ChatRoom";
+const ChatRoom = lazy(() => import("./pages/ChatRoom"));
+const DisplayName = lazy(() => import("./pages/DisplayName.jsx"));
 import SignUp from "./pages/SignUp.jsx";
-import DisplayName from "./pages/DisplayName.jsx";
 import PrivateRoutes from "./pages/PrivateRoutes.jsx";
 import ForgotPassword from "./pages/ForgotPassword.jsx";
 import Loading from "./pages/Loading.jsx";
@@ -17,19 +17,21 @@ export default function App() {
       <div className="App">
         <section className="content-wrapper">
           <Router>
-            <Routes>
-              <Route element={<PrivateRoutes />}>
-                <Route exact path="/" element={<ChatRoom />} />
-              </Route>
-              <Route exact path="/signup" element={<SignUp />} />
-              <Route exact path="/login" element={<SignIn />} />
-              <Route exact path="/displayname" element={<DisplayName />} />
-              <Route
-                exact
-                path="/forgotpassword"
-                element={<ForgotPassword />}
-              />
-            </Routes>
+            <Suspense fallback={<Loading />}>
+              <Routes>
+                <Route element={<PrivateRoutes />}>
+                  <Route exact path="/" element={<ChatRoom />} />
+                </Route>
+                <Route exact path="/signup" element={<SignUp />} />
+                <Route exact path="/login" element={<SignIn />} />
+                <Route exact path="/displayname" element={<DisplayName />} />
+                <Route
+                  exact
+                  path="/forgotpassword"
+                  element={<ForgotPassword />}
+                />
+              </Routes>
+            </Suspense>
           </Router>
         </section>
       </div>
